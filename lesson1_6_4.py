ca6b168d53c58667a67428547dadbb5b394cfc1b
Anna <Meshalnikova@gmail.com>
2020-04-24 21:10:22 +0300

    Задание: поиск элемента по XPath

---
+++ lesson1_6_4.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+
+link = 'http://suninjuly.github.io/find_xpath_form'
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    input1 = browser.find_element_by_xpath("//input[@name = 'first_name']")
+    input1.send_keys('Ivan')
+    input2 = browser.find_element_by_xpath("//input[@name = 'last_name']")
+    input2.send_keys('Petrov')
+    input3 = browser.find_element_by_xpath("//input[contains(@class,'city')]")
+    input3.send_keys('Smolensk')
+    input4 = browser.find_element_by_xpath("//input[@id='country']")
+    input4.send_keys('Russia')
+    button = browser.find_element_by_xpath("//button[@type='submit']")
+    button.click()
+    
+finally:
+    time.sleep(40)
+    browser.quit()
+    
+    
+    
